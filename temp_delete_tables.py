06266dd3b095cdddd11c026d47e07bcf17e04620
Mustapha <bpmmustapha@gmail.com>
2018-02-04 18:08:37 +1100

    added some error handling and created the database reset script

--- temp_delete_tables.py
+++ temp_delete_tables.py
@@ -1,18 +1,20 @@
-from classes import REGIONS
+from classes import REGIONS, DATABASE_DETAILS,Database, Misc
 
-def reset_database(self):
+def reset_database():
     tables = ["summoners", "averages", "champ_stats", "final_stats", "game_stats", "games"]
-
-    all_champs = list(self.database.get_all_items("Base_champ_list", "id"))
-
+    database = Database(DATABASE_DETAILS)
+    all_champs = list(database.get_all_items("Base_champ_list", "id"))
+    misc = Misc()
     for region in REGIONS:
 
         for table in tables:
-            self.database.delete_table(region + "_" + table)
-            self.database.replicate_table("Base_" + table, region + "_" + table)
-            self.misc.logging(region, "table " + region + "_" + table + " has been reset", "log")
+            database.delete_table(region + "_" + table)
+            database.replicate_table("Base_" + table, region + "_" + table)
+            misc.logging(region, "table " + region + "_" + table + " has been reset", "log")
 
         for champ in all_champs:
-            self.database.delete_table(region + "_" + str(champ))
-            self.database.replicate_table("Base_champ", region + "_" + str(champ))
-            self.misc.logging(region, "table " + region + "_" + str(champ) + " has been reset", "log")
+            database.delete_table(region + "_" + str(champ))
+            database.replicate_table("Base_champ", region + "_" + str(champ))
+            misc.logging(region, "table " + region + "_" + str(champ) + " has been reset", "log")
+
+reset_database()
